94379efacc315c4304534e8613d8c241b3c9d43d
Karim Cali <karimcali@github.com>
2025-12-09 14:59:42 +0000

    Add new Spring Boot 3 project SpaceManualImpl(Used Spring Initializr)

---
+++ SpaceManualImpl/src/main/java/ie/spring/planetsystem/SpaceManualImpl/SpaceManualImplApplication.java
@@ -0,0 +1,13 @@
+package ie.spring.planetsystem.SpaceManualImpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceManualImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceManualImplApplication.class, args);
+	}
+
+}
