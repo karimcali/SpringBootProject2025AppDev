9ab889ddc581af5968309c2d982ddddb296fb3b2
karimcali <karimcal670@gmail.com>
2025-12-10 12:05:18 +0000

    feat: Enhance entity models by adding relationships and new fields for Moon, MyUser, and Planet

--- SpaceManualImpl/src/main/java/ie/spring/planetsystem/SpaceManualImpl/entities/Moon.java
+++ SpaceManualImpl/src/main/java/ie/spring/planetsystem/SpaceManualImpl/entities/Moon.java
@@ -1,5 +1,29 @@
 package ie.spring.planetsystem.SpaceManualImpl.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "moons")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Moon {
-    
+      @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "moon_id")
+    private Long moonId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "diameter_km")
+    private Double diameterKm;
+
+    @Column(name = "orbital_period_days")
+    private Double orbitalPeriodDays;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "planet_id", nullable = false)
+    private Planet planet;
 }
